98694fd7137971dbedce6b681d7ad69122293180
Filip Krawcewicz <fkrawcewicz@gmail.com>
2021-12-29 15:06:36 +0100

    Dodanie podstawowej logiki dodawania do kolejki

---
+++ Clinic/Appointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Clinic
+{
+    internal class Appointment
+    {
+        private string patientName;
+        private DateOnly appointmentDate;
+        private string testName;
+
+        public DateOnly AppointmentDate { get { return appointmentDate; } set { appointmentDate = value; } }
+    }
+}
---
+++ Clinic/QueueManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic
+{
+    internal class QueueManager
+    {
+        private SortedDictionary<DateOnly, List<Appointment>> queue;
+
+        public void Add(Appointment appointment)
+        {
+            DateOnly date = appointment.AppointmentDate;
+            var dayQueuePair = queue.FirstOrDefault(kv => kv.Key.Equals(date), KeyValuePair.Create(date, new List<Appointment>()));
+            dayQueuePair.Value.Add(appointment);
+        }
+    }
+}
